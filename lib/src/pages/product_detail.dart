6bbd0cc48b76329a84d415db509ba1e8d7858f61
francenghia <francenghia@gmail.com>
2020-04-30 20:23:31 +0700

    init screen

--- lib/src/pages/product_detail.dart
+++ lib/src/pages/product_detail.dart
@@ -1,3 +1,9 @@
+import 'package:ecommerce/src/models/data.dart';
+import 'package:ecommerce/src/themes/colors.dart';
+import 'package:ecommerce/src/themes/theme.dart';
+import 'package:ecommerce/src/wigets/prduct_icon.dart';
+import 'package:ecommerce/src/wigets/product_card.dart';
+import 'package:ecommerce/src/wigets/title_text.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailPage extends StatefulWidget {
@@ -9,9 +15,383 @@ class ProductDetailPage extends StatefulWidget {
 
 class _ProductDetailPageState extends State<ProductDetailPage>
     with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    animation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: controller, curve: Curves.easeInToLinear));
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  bool isLiked = true;
+  Widget _appBar() {
+    return Container(
+      padding: AppTheme.padding,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: _icon(Icons.arrow_back_ios,
+                color: Colors.black54, size: 15, padding: 12, isOutLine: true),
+          ),
+          InkWell(
+            onTap: () {
+              setState(() {
+                isLiked = !isLiked;
+              });
+            },
+            child: _icon(isLiked ? Icons.favorite : Icons.favorite_border,
+                color: isLiked ? ColorsLight.red : ColorsLight.lightGrey,
+                size: 15,
+                padding: 12,
+                isOutLine: false),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _icon(IconData icon,
+      {Color color = ColorsLight.iconColor,
+        double size = 20,
+        double padding = 10,
+        bool isOutLine = false}) {
+    return Container(
+      height: 40,
+      width: 40,
+      padding: EdgeInsets.all(padding),
+      // margin: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: ColorsLight.iconColor,
+            style: isOutLine ? BorderStyle.solid : BorderStyle.none),
+        borderRadius: BorderRadius.all(Radius.circular(13)),
+        color:
+        isOutLine ? Colors.transparent : Theme.of(context).backgroundColor,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Color(0xfff8f8f8),
+              blurRadius: 5,
+              spreadRadius: 10,
+              offset: Offset(5, 5)),
+        ],
+      ),
+      child: Icon(icon, color: color, size: size),
+    );
+  }
+
+  Widget _productImage() {
+    return AnimatedBuilder(
+      builder: (context, child) {
+        return AnimatedOpacity(
+          duration: Duration(milliseconds: 500),
+          opacity: animation.value,
+          child: child,
+        );
+      },
+      animation: animation,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          TitleText(
+            text: "AIP",
+            fontSize: 160,
+            color: ColorsLight.lightGrey,
+          ),
+          Image.asset('assets/images/show_1.png')
+        ],
+      ),
+    );
+  }
+
+  Widget _categoryWidget() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 0),
+      width: AppTheme.fullWidth(context),
+      height: 80,
+      child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:
+          AppData.showThumbnailList.map((x) => _thumbnail(x)).toList()),
+    );
+  }
+
+  Widget _thumbnail(String image) {
+    return AnimatedBuilder(
+        animation: animation,
+        //  builder: null,
+        builder: (context, child) => AnimatedOpacity(
+          opacity: animation.value,
+          duration: Duration(milliseconds: 500),
+          child: child,
+        ),
+        child: Container(
+          height: 40,
+          width: 50,
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: ColorsLight.grey,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(13),
+            ),
+            // color: Theme.of(context).backgroundColor,
+          ),
+          child: Image.asset(image),
+        ));
+  }
+
+  Widget _detailWidget() {
+    return DraggableScrollableSheet(
+      maxChildSize: .8,
+      initialChildSize: .53,
+      minChildSize: .53,
+      builder: (context, scrollController) {
+        return Container(
+          padding: AppTheme.padding.copyWith(bottom: 0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+              color: Colors.white),
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                SizedBox(height: 5),
+                Container(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 50,
+                    height: 5,
+                    decoration: BoxDecoration(
+                        color: ColorsLight.iconColor,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TitleText(text: "NIKE AIR MAX 200", fontSize: 25),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              TitleText(
+                                text: "\$ ",
+                                fontSize: 18,
+                                color: ColorsLight.red,
+                              ),
+                              TitleText(
+                                text: "240",
+                                fontSize: 25,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.star,
+                                  color: ColorsLight.yellowColor, size: 17),
+                              Icon(Icons.star,
+                                  color: ColorsLight.yellowColor, size: 17),
+                              Icon(Icons.star,
+                                  color: ColorsLight.yellowColor, size: 17),
+                              Icon(Icons.star,
+                                  color: ColorsLight.yellowColor, size: 17),
+                              Icon(Icons.star_border, size: 17),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                _availableSize(),
+                SizedBox(
+                  height: 20,
+                ),
+                _availableColor(),
+                SizedBox(
+                  height: 20,
+                ),
+                _description(),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _availableSize() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TitleText(
+          text: "Available Size",
+          fontSize: 14,
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _sizeWidget("US 6"),
+            _sizeWidget("US 7", isSelected: true),
+            _sizeWidget("US 8"),
+            _sizeWidget("US 9"),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _sizeWidget(String text,
+      {Color color = ColorsLight.iconColor, bool isSelected = false}) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: ColorsLight.iconColor,
+            style: !isSelected ? BorderStyle.solid : BorderStyle.none),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color:
+        isSelected ? ColorsLight.orange : Theme.of(context).backgroundColor,
+      ),
+      child: TitleText(
+        text: text,
+        fontSize: 16,
+        color: isSelected ? ColorsLight.background : ColorsLight.titleTextColor,
+      ),
+    );
+  }
+
+  Widget _availableColor() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TitleText(
+          text: "Available Size",
+          fontSize: 14,
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _colorWidget(ColorsLight.yellowColor, isSelected: true),
+            SizedBox(
+              width: 30,
+            ),
+            _colorWidget(ColorsLight.lightBlue),
+            SizedBox(
+              width: 30,
+            ),
+            _colorWidget(ColorsLight.black),
+            SizedBox(
+              width: 30,
+            ),
+            _colorWidget(ColorsLight.red),
+            SizedBox(
+              width: 30,
+            ),
+            _colorWidget(ColorsLight.skyBlue),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _colorWidget(Color color, {bool isSelected = false}) {
+    return CircleAvatar(
+      radius: 12,
+      backgroundColor: color.withAlpha(150),
+      child: isSelected
+          ? Icon(
+        Icons.check_circle,
+        color: color,
+        size: 18,
+      )
+          : CircleAvatar(radius: 7, backgroundColor: color),
+    );
+  }
+
+  Widget _description() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TitleText(
+          text: "Available Size",
+          fontSize: 14,
+        ),
+        SizedBox(height: 20),
+        Text(AppData.description),
+      ],
+    );
+  }
+
+  FloatingActionButton _flotingButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: ColorsLight.orange,
+      child: Icon(Icons.shopping_basket,
+          color: Theme.of(context).floatingActionButtonTheme.backgroundColor),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      floatingActionButton: _flotingButton(),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xfffbfbfb),
+                  Color(0xfff7f7f7),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              )),
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  _appBar(),
+                  _productImage(),
+                  _categoryWidget(),
+                ],
+              ),
+              _detailWidget()
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
